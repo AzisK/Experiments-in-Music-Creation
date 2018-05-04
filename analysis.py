a623e18452ca741e4f79d6baf6a609cd37f37d09
AzisK <azuolas.krusna@yahoo.com>
2018-05-04 18:28:37 +0300

    Add analysis defs.

--- analysis.py
+++ analysis.py
@@ -1,1 +1,60 @@
 import matplotlib.pyplot as plt
+import midiUtils as mu
+from pathlib import Path
+import pandas as pd
+
+def generateCSV(force=False):
+  my_file = Path('music.csv')
+  if not my_file.is_file() or force: 
+    mu.readPieces()
+
+df = mu.loadPieces()
+
+def getNotesHist():
+  plt.hist(df['pitch'], bins=128)
+  plt.xlabel('Pitch')
+  plt.ylabel('Times')
+  plt.show()
+
+def getNotesRighHist():
+  mydf = df[df['hand'] == 1]
+  plt.hist(mydf['pitch'], bins=128)
+  plt.show()
+
+def getNotesLeftHist():
+  mydf = df[df['hand'] == 0]
+  plt.hist(mydf['pitch'], bins=128)
+  plt.show()
+
+def getLengthHist():
+  plt.hist(df['length'], bins=200)
+  plt.xlabel('Length')
+  plt.ylabel('Times')
+  plt.show()
+
+def getLengthQuantHist():
+  mu.quantizeDf(df)
+  plt.hist(df['length'], bins=200)
+  plt.xlabel('Length')
+  plt.ylabel('Times')
+  plt.show()
+
+def getLengthRightHist():
+  mydf = df[df['hand'] == 1]
+  plt.hist(mydf['length'], bins=200)
+  plt.show()
+
+def getLengthLeftHist():
+  mydf = df[df['hand'] == 0]
+  plt.hist(mydf['length'], bins=200)
+  plt.show()
+
+# getNotesHist()
+# getNotesRighHist()
+# getNotesLeftHist()
+
+# getLengthHist()
+# getLengthRightHist()
+# getLengthLeftHist()
+
+getLengthQuantHist()
