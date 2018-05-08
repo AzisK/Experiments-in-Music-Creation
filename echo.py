ec6c6117e34bbb712ce961682b85c198638bb80c
AzisK <azuolas.krusna@yahoo.com>
2018-05-08 15:24:34 +0300

    Optimize the esn and add more statistics

--- echo.py
+++ echo.py
@@ -26,76 +26,112 @@ def generateNotes(Y, t, y, noteLength):
         y[index] = 0
         noteLength = 0
 
-def predict(lr = 0.8, ins = 1, sr = 1.25, reg=1):
-    inSize = outSize = 128
-    resSize = 1000
+def predict(minp=0, maxp=127, lr = 0.5, ins = 0.65, sr = 0.75, reg=1, size=1000, noise=0):
+    inSize = outSize = maxp + 1 - minp
+    resSize = size
 
-    np.random.seed(123)
+    # Pick only the necessary elements
+    # data = data[:, minp : maxp + 1]
 
-    trainLen = round(len(data) * 0.8)
-    testLen = len(data) - trainLen
-    initLen = 120
+    # np.random.seed(123)
+
+    lengths = np.shape(data)[1]
+    train = round(lengths * 0.8)
+    test = lengths - train
+    init = 300
 
     Win = (np.random.rand(resSize, 1 + inSize) - 0.5) * ins
-    W = np.random.rand(resSize, resSize) - 0.5 
+    W = np.random.rand(resSize, resSize) - 0.5
 
     print('Computing spectral radius...')
-    rhoW = max(abs(lin.eig(W)[0]))
 
-    W *= sr / rhoW 
+    rhoW = max(abs(np.linalg.eigvals(W)))
+    print('Maximum eigen-value: {}'.format(rhoW))
+
+    W *= sr / rhoW
     print('Done! Spectral radius / rhoW: {0}'.format(sr / rhoW))
 
     # Allocated memory for the design (collected states) matrix
-    X = np.zeros((1 + inSize + resSize, trainLen - initLen))
+    X = np.zeros((train - init, 1 + inSize + resSize))
 
     # Set the corresponding target matrix directly
-    Yt = data[initLen + 1 : trainLen + 1]
+    Yt = data[:, init + 1 : train + 1]
 
     # Run the reservoir with the data and collect X
     x = np.zeros((resSize, 1))
 
     print('Starting training...')
-    for t in range(trainLen):
-        u = data[t][:].reshape(-1, 1)
-        x = (1 - lr) * x + a * np.tanh( np.dot( Win, np.vstack((1, u)) ) + np.dot( W, x ) )
-        if t >= initLen:
-            X[:, t - initLen] = np.vstack((1, u, x))[:, 0]
+    for t in range(train):
+        u = np.array([data[:, t]]).transpose()
+        inps = np.dot(Win, np.vstack((1, u)))
+        x = (1 - lr) * x + lr * np.tanh(inps + np.dot(W, x))
+
+        if noise:
+            noise = np.random.rand(resSize, 1)
+            x += noise
+
+        if t >= init:
+            X[t - init, :] = np.vstack((1, u, x)).transpose()
+
+    print('Iterations done :)')
 
     # Train the output
-    X_T = X.T
-    Wout = np.dot( np.dot(Yt.T, X_T), lin.inv( np.dot(X, X_T) + reg * np.eye(1 + inSize + resSize)))
+    inv = lin.inv(np.dot(X.T, X) + reg * np.eye(1 + inSize + resSize))
+    fdback = np.dot(Yt, X)
+
+    print('Computing Wout...')
+    Wout = np.dot(fdback, inv)
+
     print('Training done!')
 
     # Generate output matrix
-    Y = np.zeros((outSize, testLen))
+    Y = np.zeros((outSize, test))
 
-    u = data[trainLen].reshape(-1, 1)
+    u = np.array([data[:, train]]).transpose()
 
     # noteLength = 0
 
     print('Starting testing!')
-    for t in range(testLen):
-        x = (1 - a) * x + a * np.tanh(np.dot( Win, np.vstack((1, u)) ) + np.dot(W, x))
-        y = np.dot( Wout, np.vstack((1, u, x)) )
+    for t in range(test):
+        inps = np.dot(Win, np.vstack((1, u)))
+        x = (1 - lr) * x + lr * np.tanh(inps + np.dot(W, x))
+
+        if noise:
+            noise = np.random.rand(resSize, 1)
+            x += noise
+
+        y = np.dot(Wout, np.vstack((1, u, x)))
 
         # generateNotes(Y, t, y, noteLength)
 
-        Y[:,t] = y.T
-        # Generative mode:
+        Y[:, t] = y.transpose()
+        
+        # GENERATIVE:
         # u = y
-        # This would be a predictive mode:
-        u = data[trainLen + t].reshape(-1, 1)
+
+        # PREDICTIVE:
+        u = np.array([data[:, train + t]]).transpose()
+
     print('Testing done!')
 
-    # Compute MSE for the first errorLen time steps
-    se = np.square(data[trainLen: trainLen + testLen] - Y[:, 0 : testLen].T)
+    # Compute MEAN, RMSE & STANDARD DEVATION
+    mean = Y.mean()
+
+    diff = data[:, train: train + test + 1] - Y[:, 0 : test]
+    se = np.square(diff)
     rmse = np.mean(np.sqrt(se))
-    print("Error: {}".format(rmse))
-    return rmse
+
+    coldiff = Y - np.array([Y.mean(1)]).transpose()
+    std = np.mean(np.sqrt(np.square(coldiff)))
+    print("MEAN: {0}, RMSE: {1}, STD: {2}".format(mean, rmse, std))
+    return mean, rmse, std
+
+    # OUTPUT
     # return Y.T
 
 df = mu.loadPieces()
-data = mu.toStateMatrix(df)
+mu.quantizeDf(df)
+data = mu.toStateMatrix(df, 29, 91)
 
 def gridSearch():
     print('Starting grid search for optimal values for the echo network...')
@@ -104,14 +140,17 @@ def gridSearch():
         for ins in np.linspace(0.2, 2, 4, endpoint=True):
             for sr in np.linspace(0.2, 2, 4, endpoint=True):
                 for reg in np.logspace(-2, -2, 5, endpoint=True):
-                    print('*')
-                    print('Calculating the error for lr={0}, ins={1}, sr={2}'.format(lr, ins, sr, reg))
-                    gs.append((lr, ins, sr, reg, predict(lr, ins, sr, reg)))
-                    gsd = pd.DataFrame(gs, columns=['lr', 'ins', 'sr', 'reg', 'error'])
+                    for size in np.linspace(1000, 2000, 2, endpoint=True):
+                        for noise in np.linspace(0, 1, 2, endpoint=True):
+                        print('*')
+                        print('Calculating the error for:')
+                        print('*** lr={0}, ins={1}, sr={2}, reg={3}, size={3}, noise={4}'.format(lr, ins, sr, reg, size, noise))
+
+                        gs.append((lr, ins, sr, reg, size, predict(29, 91, lr, ins, sr, reg, size, noise)))
+                        gsd = pd.DataFrame(gs, columns=['lr', 'ins', 'sr', 'reg', 'size', 'noise', 'error'])
 
     print('Grid search done!')
     gsd.to_csv('gridsearch.csv')
 
 gridSearch()
-# error = predict()
-# print(error)
+# mean, rmse, std = predict(29, 91)
