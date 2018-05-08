3322b0614a96363088b8c6c01a8f4114dc484a23
AzisK <azuolas.krusna@yahoo.com>
2018-05-08 15:26:43 +0300

    Errors analysis code

--- errorAnalysis.py
+++ errorAnalysis.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
 import numpy as np
 
 df = pd.read_csv('error.csv')
@@ -10,6 +13,8 @@ def getError():
 	A = A[np.where(A > 0)]
 
 	plt.hist(A, bins=150)
+	plt.xlabel('Error')
+	plt.ylabel('Times')
 	plt.show()
 
 def getErrorRough():
@@ -18,6 +23,8 @@ def getErrorRough():
 	A = A[np.where(A > 0)]
 
 	plt.hist(A, bins=150)
+	plt.xlabel('Error')
+	plt.ylabel('Times')
 	plt.show()
 
 def getTop():
@@ -25,9 +32,30 @@ def getTop():
 	print(dfSorted)
 
 def groupInput():
-	# A = df.replace([np.inf, -np.inf], np.nan)
-	# A = A.fillna(1000000)
 	A = df.groupby('input scaling')['error'].mean()
-	print(A)
+	A.to_csv('errorGroupByInput.csv')
 
-groupInput()
+def groupInputAndRadius():
+	A = df.groupby(['input scaling', 'spectral radius'])['error'].mean()
+	A.to_csv('errorGroupByInputAndRadius.csv')
+
+def plot3d(csv):
+	df = pd.read_csv(csv)
+	df.loc[df['error'] > 0.1, 'error'] = 0.1
+	fig = plt.figure()
+	ax = Axes3D(fig)
+	X = df.iloc[:,0].values
+	Y = df.iloc[:,1].values
+	Z = df.iloc[:,2].values
+	surf = ax.plot_trisurf(X, Y, Z, cmap=cm.jet, linewidth=1, antialiased=False)
+	fig.colorbar(surf, shrink=0.5, aspect=5)
+	ax.set_xlabel('Input scaling')
+	ax.set_ylabel('Spectral radius')
+	ax.set_zlabel('Mean error')
+	plt.show()
+
+# getError()
+# getErrorRough()
+# groupInput()
+# groupInputAndRadius()
+plot3d('errorGroupByInputAndRadius.csv')
