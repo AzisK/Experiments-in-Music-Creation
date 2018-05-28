74c1b83785cc3955f532809391744c6077b7f06a
AzisK <azuolas.krusna@yahoo.com>
2018-05-28 20:41:19 +0300

    Add sigmoid function, add a prediction function based on sampling as well as some musical rules

--- echo.py
+++ echo.py
@@ -4,30 +4,85 @@ import pandas as pd
 import scipy.linalg as lin
 import midiUtils as mu
 from pathlib import Path
+from operator import itemgetter
 
-def predictNotes(y, deg):
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def predictNotes(y, deg, noteLengths, outSize):
+    mnotes = []
+    pnotes = []
+
     for index, value in enumerate(y):
-        if coldiff[index] > np.power(value, deg):
-            y[index] = 1
+        if noteLengths[index] > 1:
+            diff = np.power(abs(value), 1 + (deg - 1) / ((noteLengths[index] % 8) + 1)) - np.random.rand()
+        elif noteLengths[index]:
+            diff = np.power(abs(value), 1 + (deg - 1) / 2) - np.random.rand()
         else:
-            y[index] = 0
+            diff = np.power(abs(value), deg) - np.random.rand()
 
-def generateNotes(Y, t, y, noteLength):
-    degree = 0.8
+        if diff > 0:
+            if value > 0:
+                pnotes.append((index, diff))
+            elif value < 0:
+                mnotes.append((index, diff))
+        # if np.power(abs(value), deg) > np.random.rand():
+        # if abs(value) > np.random.rand():
 
-    if value > np.random.rand() ** degree:
-        y[index] = 1
-    elif t > 0 and Y[index, t-1] == 1:
-        noteLength += 1
-        addition = (1 - degree) / noteLength + degree
+        #     if value > 0:
+        #         y[index] = 1
+        #     else:
+        #         y[index] = -1
+        # else:
+        #     y[index] = 0
 
-        if value > np.random.rand() ** addition:
-            y[index] = 1
-    else:
-        y[index] = 0
-        noteLength = 0
+    y[:] = 0
+    newNoteLengths = np.zeros((outSize))
 
-def predict(minp=0, maxp=127, lr = 0.75, ins = 0.2, sr = 0.2, regs=[100], degs=(1, 1, 1)):
+    pnotes.sort(key=itemgetter(1), reverse=True)
+    mnotes.sort(key=itemgetter(1), reverse=True)
+
+    high = []
+    if pnotes:
+        for note in pnotes:
+            if not high:
+                high.append(note[0])
+            if high:
+                for h in high:
+                    chord = abs(h - note[0])
+                    if chord == 3 or chord == 4:
+                        high.append(note[0])
+            if len(high) == 6:
+                break
+
+    for note in high:
+        y[note] = 1
+        newNoteLengths[note] = 1
+        noteLengths += 1
+            
+    high = []
+    if mnotes:
+        for note in mnotes:
+            if not high:
+                high.append(note[0])
+            if high:
+                for h in high:
+                    chord = abs(h - note[0])
+                    if chord == 3 or chord == 4:
+                        high.append(note[0])
+            if len(high) == 6:
+                break        
+
+    for note in high:
+        y[note] = -1
+        newNoteLengths[note] = 1
+        noteLengths += 1
+
+    for i, n in enumerate(newNoteLengths):
+        if not n:
+            noteLengths[i] = 0
+
+def predict(minp=0, maxp=127, lr = 0.02, ins = 0.00023, sr = 0.02, regs=[0.1], degs=(1, 1, 1)):
     inSize = outSize = maxp + 1 - minp
     resSize = 1000
     print('Reservoir size is: {}'.format(resSize))
@@ -43,7 +98,12 @@ def predict(minp=0, maxp=127, lr = 0.75, ins = 0.2, sr = 0.2, regs=[100], degs=(
     init = 300
 
     Win = (np.random.rand(resSize, 1 + inSize) - 0.5) * ins
-    W = np.random.rand(resSize, resSize) - 0.5
+    W = np.random.rand(resSize, resSize) - 0.5 
+
+    # for i in range(len(W)):
+    #     for j in range(len(W[i])):
+    #         if np.random.rand() < 0.1:
+    #             W[i, j] = 0
 
     print('Computing spectral radius...')
 
@@ -93,10 +153,8 @@ def predict(minp=0, maxp=127, lr = 0.75, ins = 0.2, sr = 0.2, regs=[100], degs=(
         Y = np.zeros((outSize, test))
 
         for deg in np.linspace(degs[0], degs[1], degs[2], endpoint=True):
-            print('Quantizing the notes with the degree of: {}'.format(deg))
-
             u = np.array([data[:, train]]).transpose()
-            # noteLength = 0
+            noteLengths = np.zeros((outSize))
 
             for t in range(test):
                 inps = np.dot(Win, np.vstack((1, u)))
@@ -108,7 +166,7 @@ def predict(minp=0, maxp=127, lr = 0.75, ins = 0.2, sr = 0.2, regs=[100], degs=(
 
                 y = np.dot(Wout, np.vstack((1, u, x)))
 
-                predictNotes(y, deg)
+                predictNotes(y, deg, noteLengths, outSize)
 
                 Y[:, t] = y.transpose()
 
@@ -122,6 +180,7 @@ def predict(minp=0, maxp=127, lr = 0.75, ins = 0.2, sr = 0.2, regs=[100], degs=(
             mean = Y.mean()
 
             diff = data[:, train: train + test + 1] - Y[:, 0 : test]
+            # diff = data[:, train: train + test + 1]
             se = np.square(diff)
             rmse = np.mean(np.sqrt(se))
 
@@ -131,28 +190,34 @@ def predict(minp=0, maxp=127, lr = 0.75, ins = 0.2, sr = 0.2, regs=[100], degs=(
             rers.append((r, mean, rmse, std, deg))
 
             # OUTPUT
-            # return Y
+            return Y
 
-    return rers
+    # return rers
 
 def gridSearch():
     print('Starting grid search for optimal values for the echo network...')
     gs = []
-    for lr in np.linspace(0.25, 1, 4, endpoint=True):
-        for ins in np.linspace(0.2, 2, 4, endpoint=True):
-            for sr in np.linspace(0.2, 2, 4, endpoint=True):
+    for lr in np.linspace(0.02, 0.02, 1, endpoint=True):
+    # for lr in [0.025]:
+        for ins in np.linspace(0.000223, 0.00025, 3, endpoint=True):
+        # for ins in [0.00002]:
+            for sr in np.linspace(0.02, 0.02, 1, endpoint=True):
+            # for sr in [0.1]:
                 print('*')
                 print('Calculating the error for:')
                 print('*** lr={0}, ins={1}, sr={2}'.format(lr, ins, sr))
-                regs = [0.01, 0.1, 1, 10, 100]
+                regs = [1, 0.1]
                 rers = predict(29, 91, lr, ins, sr, regs)
                 for rer in rers:
                     reg, mean, rmse, std, deg = rer
                     gs.append((lr, ins, sr, reg, mean, rmse, std, deg))
+                    # gs.append((lr, ins, sr, reg, mean, rmse, std))
+
                     gsd = pd.DataFrame(gs, columns=['lr', 'ins', 'sr', 'reg', 'mean', 'rmse', 'std', 'deg'])
+                    # gsd = pd.DataFrame(gs, columns=['lr', 'ins', 'sr', 'reg', 'mean', 'rmse', 'std'])
 
     print('Grid search done!')
-    gsd.to_csv('gridsearch.csv')
+    gsd.to_csv('gridsearch.hand.3.csv', index=False)
 
 def getColdiff():
     my_file = Path('coldiff.npy')
@@ -166,10 +231,14 @@ def getColdiff():
 df = mu.loadPieces()
 mu.quantizeDf(df)
 data = mu.toStateMatrix(df, 29, 91)
-coldiff = getColdiff()
+
+# coldiff = getColdiff()
+# print(coldiff)
 
 # gridSearch()
-# out = predict(29, 91, degs=(1, 1.5, 5))
-out = predict(29, 91, regs=[1, 100], degs=(1, 2, 3))
-# tuples = mu.state2Tuples(out, 29)
-# mu.tuples2Midi(tuples)
+out = predict(29, 91, degs=(1, 1, 1))
+# print(out)
+# out = predict(29, 91, regs=[1, 100], degs=(1, 1.4, 5))
+# out = predict(29, 91, regs=[100], degs=(1.125, 1.125, 1))
+tuples = mu.state2Tuples(out, 29, 60)
+# mu.tuples2Midi(tuples, 'Midi1.05.mid')
